edb807b18300159243dc691c4340983f0f41222d
satorusasozaki <sasozakisatoru@gmail.com>
2015-09-14 14:51:28 -0700

    Change to the totalSecond way

--- Loops/clock.cpp
+++ Loops/clock.cpp
@@ -8,34 +8,44 @@
 #include <sstream>
 #include <iomanip>
 using namespace std;
+
+void displayTime(int totalSeconds) {
+
+    for (totalSeconds; ;totalSeconds++) {
+        int hours = totalSeconds / (60 * 60);
+        int minutes = (totalSeconds - hours * 60 * 60) / 60;
+        int seconds = totalSeconds - hours * 60 * 60 - minutes * 60;
+        cout << "Time now: ";
+        cout  << setfill(' ')<< setw(2) << right << hours << ':';
+        cout  << setfill(' ')<< setw(2) << right << minutes << ':';
+        cout  << setfill(' ')<< setw(2) << right  << seconds<<'\r';
+        cout.flush();
+        this_thread::sleep_for (chrono::seconds(1));
+
+    }
+
+
+}
+
 int main ()
 {
-  chrono::system_clock::time_point today = chrono::system_clock::now();
-  time_t tt;
-  tt = chrono::system_clock::to_time_t ( today );
-  string t = ctime(&tt);
-  string garbage;
-  string clock;
-  stringstream(t) >> garbage >> garbage >> garbage >> clock;
+    chrono::system_clock::time_point today = chrono::system_clock::now();
+    time_t tt;
+    tt = chrono::system_clock::to_time_t ( today );
+    string t = ctime(&tt);
+    string garbage;
+    string clock;
+    stringstream(t) >> garbage >> garbage >> garbage >> clock;
 
-  int hours, minutes, seconds;
-  char c;
+    int hours, minutes, seconds;
+    char c;
 
-  stringstream(clock) >> hours >> c >>minutes>> c >> seconds;
+    stringstream(clock) >> hours >> c >>minutes>> c >> seconds;
 
-  for (; hours < 24; hours++) {
-        for (; minutes < 60; minutes++ ) {
-              for (; seconds < 60; seconds++) {
-                    cout << "Time now: ";
-                    cout  << setfill(' ')<< setw(2) << right << hours << ':';
-                    cout  << setfill(' ')<< setw(2) << right << minutes << ':';
-                    cout  << setfill(' ')<< setw(2) << right  << seconds<<'\r';
-                    cout.flush();
-                    this_thread::sleep_for (chrono::seconds(1));
-              }
-              seconds = 0;
-        }
-        minutes = 0;
-  }
-  return 0;
+    int totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+
+    displayTime(totalSeconds);
+
+    return 0;
+
 }
