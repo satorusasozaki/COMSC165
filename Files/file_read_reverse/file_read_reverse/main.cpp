45122512c65118e43881277939a11b7e77abb96d
Satoru Sasozaki <sasozakisatoru@gmail.com>
2015-10-05 11:03:09 -0700

    Add the function lab

---
+++ Files/file_read_reverse/file_read_reverse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  file_read_reverse
+//
+//  Created by Satoru Sasozaki on 9/24/15.
+//  Copyright © 2015 Satoru Sasozaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ functions_test/functions_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  functions_test
+//
+//  Created by Satoru Sasozaki on 9/30/15.
+//  Copyright © 2015 Satoru Sasozaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
