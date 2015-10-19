3ff48405b004cb7021dcaaba2e0a1ed59aa1975d
Satoru Sasozaki <sasozakisatoru@gmail.com>
2015-10-19 14:07:11 -0700

    Reorgranized the folder structure adding class_object lab

---
+++ vector_lab/vector_lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vector_lab
+//
+//  Created by Satoru Sasozaki on 10/12/15.
+//  Copyright © 2015 Satoru Sasozaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
